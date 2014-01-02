22908b7a4007de9d5a3cc53c168ff0845cf7ae15
Lukasz Szajkowski <lukasz@logicallysoftware.co.uk>
2014-01-02 14:36:04 +0000

    Add sample code

---
+++ my_package/models.py
@@ -0,0 +1,19 @@
+
+
+def add_some_values(value1, value2):
+    result = value1 + value2
+    return result
+
+
+class Member(object):                               # Always inherit from object
+    'Common base class for all members'             # Document your classes
+    member_count = 0                                # Public Static variable
+
+    def __init__(self, forname, refno):             # Constructor
+        self.forname = forname                      # Public variable
+        self.__refno = refno                        # Private variable
+        Member.member_count += 1
+
+    def get_refno(self):                            # Public method
+        return self.__refno
+
---
+++ my_script.py
@@ -0,0 +1,8 @@
+import sys									            # Import Python system library
+from my_package.models import add_some_values, Member   # Import our models
+
+if __name__ == "__main__":						        # This is equivalent to main() in C++
+    a = Member('John','0001')
+    b = add_some_values(2, 2)
+
+    print ("%s has £%d" %(a.get_refno(), b) )
