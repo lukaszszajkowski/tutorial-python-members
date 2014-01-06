c959a97c9a2ccffb99c74d6f47c5ef5ad29974be
Lukasz Szajkowski <lukasz.szajkowski@towerswatson.com>
2014-01-06 10:04:07 +0000

    some comment

--- my_script.py
+++ my_script.py
@@ -4,5 +4,3 @@ from my_package.models import add_some_values, Member   # Import our models
 if __name__ == "__main__":						        # This is equivalent to main() in C++
     a = Member('John','0001')
     b = add_some_values(2, 2)
-
-    print ("%s has £%d" %(a.get_refno(), b) )
